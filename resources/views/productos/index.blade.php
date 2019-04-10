e9747474d4be9805db144c90ccf9989cac9f3f0d
Sergio Ayala <sergio.ayala@agibiz.cl>
2019-04-10 13:08:55 -0400

    actualizacion

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
-
 @section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
